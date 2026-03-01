c3eb78f0389b7e45a26e43a44d8634ae0d539d32
Florent <florent.git@zeteo.me>
2026-03-01 16:21:26 +0100

    Fix permissions on config.yaml

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -21,16 +21,15 @@ apply_permissions() {
         # Get all entries between "permissions:" and "relay:" keys, remove the role part, remove commented parts, format it with newlines and clean whitespaces and double quotes.
         allDefinedEntries=$(awk '/permissions:/{flag=1; next} /relay:/{flag=0} flag' "$install_dir/config.yaml" | sed "/: $role/d" | sed -r 's/: (admin|user|relay)//' | tr -d '[:blank:]' | sed '/^#/d' | tr -d '\"' | tr ',' '\n' )
         # Delete everything from the corresponding role to insert the new defined values. This way we also handle deletion of users.
-        sed -i "/permissions:/,/relay:/{/: $role/d;}" "$install_dir/config.yaml"
+        ynh_exec_as_app sed -i "/permissions:/,/relay:/{/: $role/d;}" "$install_dir/config.yaml"
         # Ensure that entries with value surrounded with quotes are deleted too. E.g. "users".
-        sed -i "/permissions:/,/relay:/{/: \"$role\"/d;}" "$install_dir/config.yaml"
+        ynh_exec_as_app sed -i "/permissions:/,/relay:/{/: \"$role\"/d;}" "$install_dir/config.yaml"
       	for user in "${usersArray[@]}"
             do
-              if grep -q -x "${user}" <<< "$allDefinedEntries"
-              then
+              if grep -q -x "${user}" <<< "$allDefinedEntries"; then
                 ynh_print_info "User $user already defined in another role."
               else
-                sed -i "/permissions:/a \        \\\"$user\": $role" "$install_dir/config.yaml" # Whitespaces are needed so that the file can be correctly parsed
+                ynh_exec_as_app sed -i "/permissions:/a \        \\\"$user\": $role" "$install_dir/config.yaml" # Whitespaces are needed so that the file can be correctly parsed
               fi
         done
     fi
