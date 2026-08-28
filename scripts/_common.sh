a5e955204207bbda555847470945a337939e1be9
Florent <florent.git@zeteo.me>
2026-08-28 20:45:29 +0200

    Simplify the packaging thanks to jinja

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,53 +8,43 @@ get_synapse_db_name() {
 	ynh_app_setting_get --app="$1" --key=db_name
 }
 
-apply_permissions() {
-    set -o noglob # Disable globbing to avoid expansions when passing * as value.
-    declare values="list$role"
-    newValues="${!values}" # Here we expand the dynamic variable we created in the previous line. ! Does the trick
-    newValues="${newValues//\"}"
-    usersArray=(${newValues//,/ }) # Split the values using comma (,) as separator.
+# Before we used jinja, permissions would have to be set by the user in the config file.
+# Only that part of the file were kept, the rest were overwritten.
+# Now that we use jinja, extract the permissions of the config file
+# and store it in the settings (the source of truth).
+migrate_permissions_to_appsettings() {
+    local permissions
+    permissions=$(python3 <<EOF
+import json
+import yaml
+with open("${install_dir}/config.yaml", "r") as file:
+    config = yaml.safe_load(file)
 
-    if [ -n "$newValues" ]
-    then
-        #ynh_systemctl --action=stop
-        # Get all entries between "permissions:" and "relay:" keys, remove the role part, remove commented parts, format it with newlines and clean whitespaces and double quotes.
-        allDefinedEntries=$(awk '/permissions:/{flag=1; next} /relay:/{flag=0} flag' "$install_dir/config.yaml" | sed "/: $role/d" | sed -r 's/: (admin|user|relay)//' | tr -d '[:blank:]' | sed '/^#/d' | tr -d '\"' | tr ',' '\n' )
-        # Delete everything from the corresponding role to insert the new defined values. This way we also handle deletion of users.
-        ynh_exec_as_app sed -i "/permissions:/,/relay:/{/: $role/d;}" "$install_dir/config.yaml"
-        # Ensure that entries with value surrounded with quotes are deleted too. E.g. "users".
-        ynh_exec_as_app sed -i "/permissions:/,/relay:/{/: \"$role\"/d;}" "$install_dir/config.yaml"
-      	for user in "${usersArray[@]}"
-            do
-              if grep -q -x "${user}" <<< "$allDefinedEntries"; then
-                ynh_print_info "User $user already defined in another role."
-              else
-                ynh_exec_as_app sed -i "/permissions:/a \        \\\"$user\": $role" "$install_dir/config.yaml" # Whitespaces are needed so that the file can be correctly parsed
-              fi
-        done
-    fi
-    set +o noglob
+# Reverse value and keys so we can create simpler jq queries
+users_by_perm = dict()
+for key, value in users_by_perm:
+    my_inverted_dict.setdefault(value, list()).append(key)
 
-    ynh_print_info "Users with role $role added in $install_dir/config.yaml"
+print(json.dumps(my_inverted_dict))
+EOF
+    )
+
+    listrelay="$(jq '.relay | join(",")' <<< "$permissions")"
+    ynh_app_setting_set --key=listrelay --value="$listrelay"
+
+    listuser="$(jq '.user | join(",")' <<< "$permissions")"
+    ynh_app_setting_set --key=listuser --value="$listuser"
+
+    listadmin="$(jq '.admin | join(",")' <<< "$permissions")"
+    ynh_app_setting_set --key=listadmin --value="$listadmin"
 }
 
-set__listuser() {
-  role="user"
-  ynh_app_setting_set --key=listuser --value="$listuser"
-  apply_permissions
-  ynh_store_file_checksum "$install_dir/config.yaml"
-}
-
-set__listrelay() {
-  role="relay"
-  ynh_app_setting_set --key=listrelay --value="$listrelay"
-  apply_permissions
-  ynh_store_file_checksum "$install_dir/config.yaml"
-}
-
-set__listadmin() {
-  role="admin"
-  ynh_app_setting_set --key=listadmin --value="$listadmin"
-  apply_permissions
-  ynh_store_file_checksum "$install_dir/config.yaml"
+rename_setting() {
+    local old_name=$1
+    local new_name=$2
+    local value
+    value=$(ynh_app_setting_get --key="$old_name")
+    ynh_app_setting_set --key="$new_name" --value="$value"
+    ynh_app_setting_delete --key="$old_name"
+    eval "$old_name=\$value"
 }
